13d669b95e90d5b4ecadff0850c17c452aad4ea0
1ouiss <ls.bouet@gmail.com>
2024-04-30 11:41:59 +0200

    remove infinite loop

--- src/order/presentation/components/GetOrdersViewModel.tsx
+++ src/order/presentation/components/GetOrdersViewModel.tsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GetOrderService } from "../../domain/use-case/get-orders-service";
 
 export const GetOrdersViewModel = () => {
   const [orders, setOrders] = useState([]);
 
-  const useGetOrders = async () => {
+  useEffect(() => {
+    GetOrders();
+  }, []);
+
+  const GetOrders = async () => {
     const response = await GetOrderService();
     setOrders(response);
     console.log(response);
 
     return orders;
   };
-  useGetOrders();
 
   return {
     orders,
